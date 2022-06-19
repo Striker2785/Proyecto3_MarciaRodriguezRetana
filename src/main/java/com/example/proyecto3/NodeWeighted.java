989f76b5c2303896090494e7629664d852df45b2
JPampers <jp99@estudiantec.cr>
2022-06-18 22:11:54 -0600

    Agrega propiedades de posiciones

--- src/main/java/com/example/proyecto3/NodeWeighted.java
+++ src/main/java/com/example/proyecto3/NodeWeighted.java
@@ -6,16 +6,28 @@ public class NodeWeighted {
     // The int n and String name are just arbitrary attributes
     // we've chosen for our nodes these attributes can of course
     // be whatever you need
-    int n;
     String name;
+    int x;
+    int y;
+    int n;
     private boolean visited;
     LinkedList<EdgeWeighted> edges;
 
-    NodeWeighted(int n, String name) {
+    NodeWeighted(int n, String name, int x, int y) {
         this.n = n;
         this.name = name;
+        this.x = x;
+        this.y = y;
         visited = false;
         edges = new LinkedList<>();
+    }
+
+    int getX(){
+        return x;
+    }
+
+    int getY(){
+        return y;
     }
 
     boolean isVisited() {
