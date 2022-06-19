373059d0fb266fea4eb0d666cece43aa2753da16
JPampers <jp99@estudiantec.cr>
2022-06-18 22:12:18 -0600

    Cambia tamaño de ventana

--- src/main/java/com/example/proyecto3/HelloApplication.java
+++ src/main/java/com/example/proyecto3/HelloApplication.java
@@ -4,7 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.graphstream.graph.*;
+//import org.graphstream.graph.*;
+
 
 import java.io.IOException;
 
@@ -12,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
