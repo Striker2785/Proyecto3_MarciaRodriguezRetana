0c8f59d1a465fec78ab07a19718f69f4d46b149d
JPampers <jp99@estudiantec.cr>
2022-06-20 05:29:28 -0600

    Actuliazacion de paquetes para xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.proyecto3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires gs.core;
+    requires java.xml;
+    requires java.logging;
 
 
     opens com.example.proyecto3 to javafx.fxml;
