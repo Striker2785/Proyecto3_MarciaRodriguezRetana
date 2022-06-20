65d7704448e8394fa25438486b214adc83bba52b
JPampers <jp99@estudiantec.cr>
2022-06-20 05:32:49 -0600

    Cambio del path utilizando nodos que tienen comida

--- src/main/java/com/example/proyecto3/HelloController.java
+++ src/main/java/com/example/proyecto3/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.proyecto3;
 
+import javafx.animation.Animation;
 import javafx.animation.PathTransition;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -35,23 +37,50 @@ public class HelloController implements Initializable{
     private Button botonEmpezar;
     @FXML
     private Label textMensaje;
+    @FXML
+    private ImageView comida;
 
 
-    public void poneComida(ActionEvent event){
-
-    }
-
-
+    /**
+     * Esta funcion crea un grafo con la cantidad de nodos que el usuario determine. El programa tendra una variable de
+     * probabilidad de 0.9 para asegurarse que es un grafo conectado. Para cada nodo se crean botones y un EventHandler
+     * para dicho boton.
+     * @param event
+     * @throws IOException
+     */
     public void addButton(ActionEvent event) throws IOException {
-        gamePane.getChildren().clear();
+        gamePane.getChildren().clear(); // Es importante borrar los nodos de la partida anterior
         int numeroNodos = Integer.parseInt(cantidadNodos.getText());
-        graphWeighted = GraphShow.creaGrafo(numeroNodos, 0.3);
+        graphWeighted = GraphShow.creaGrafo(numeroNodos, 0.9);
         for (NodeWeighted node: graphWeighted.nodes) {
-            System.out.println(node.getX());
             Button newButton = new Button(node.name);
             newButton.setLayoutX(node.getX());
             newButton.setLayoutY(node.getY());
             newButton.setTextFill(Color.WHITE);
+
+            EventHandler<ActionEvent> poneComida = new EventHandler<>() {
+                /**
+                 * Se propone una condicion donde solo un nodo puede tener la propiedad de comida como true
+                 * En caso de que se cumpla la condicion, se agrega un nodo imagen de la comida y se cambia el estado
+                 * de tieneComida del nodo a true
+                 * @param event
+                 */
+                @Override
+                public void handle(ActionEvent event) {
+                    NodeWeighted nodeAccessed = graphWeighted.obtenerNodo(newButton.getText());
+                    if (graphWeighted.nodosConComida() <= 1) {
+                        nodeAccessed.setComida(true);
+                        gamePane.getChildren().add(comida);
+                        comida.setLayoutX(nodeAccessed.getX());
+                        comida.setLayoutY(nodeAccessed.getY());
+                    } else {
+                        textMensaje.setText("No mas comida");
+                    }
+                }
+            };
+
+            newButton.setOnAction(poneComida);
+
             newButton.setStyle(
                     "-fx-background-radius: 5em; " +
                             "-fx-min-width: 20px; " +
@@ -62,6 +91,7 @@ public class HelloController implements Initializable{
                             "-fx-background-insets: 0px; " +
                             "-fx-padding: 0px;"
             );
+            // se dibujan las aristas
             for(EdgeWeighted edge: node.edges){
                 Line newLine = new Line(node.getX()+10, node.getY()+10, edge.destination.getX()+10, edge.destination.getY()+10);
                 gamePane.getChildren().add(newLine);
@@ -74,38 +104,53 @@ public class HelloController implements Initializable{
         botonEmpezar.setDisable(false);
     }
 
-    public void comienzaJuego(ActionEvent event)throws IOException{
+    /**
+     * Funcion que arranca el movimiento de la hormiga a traves del grafo una vez se haya puesto la comida
+     * @param event
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public void comienzaJuego(ActionEvent event) throws IOException, InterruptedException {
         gamePane.getChildren().add(greenAnt);
-        int numeroNodos = Integer.parseInt(cantidadNodos.getText());
-        NodeWeighted start = graphWeighted.obtenerNodo(0);
-        NodeWeighted end = graphWeighted.obtenerNodo(numeroNodos-1);
-        ArrayList<NodeWeighted> pathNodos = graphWeighted.DijkstraShortestPath(start, end);
-        int size = pathNodos.size();
-        Path path = new Path();
+        NodeWeighted start = graphWeighted.obtenerNodo(0); //inicia con el primer nodo de la lista
+        NodeWeighted end = graphWeighted.nodoConComida(); // el nodo objetivo es el que tiene comida
 
-        MoveTo moveTo = new MoveTo(start.getX()-90, start.getY()-110);
-        path.getElements().add(moveTo);
+        while(true){
+            //se obtiene la lista del camino a seguir segun el algoritmo de Dijkstra
+            ArrayList<NodeWeighted> pathNodos = graphWeighted.DijkstraShortestPath(start, end);
+            int size = pathNodos.size();
+            Path path = new Path();
 
-        if(size == 0){
-            textMensaje.setText("No hay conexion entre los nodos");
-        }else{
-            for(int i = 1; i <= size; i++) {
-                int coordX = pathNodos.get(size - i).getX() - 90;
-                int coordY = pathNodos.get(size - i).getY() - 110;
-                LineTo newLine = new LineTo(coordX, coordY);
-                path.getElements().add(newLine);
+            // Coordenadas iniciales de la hormiga
+            MoveTo moveTo = new MoveTo(start.getX()-210, start.getY()-550);
+            path.getElements().add(moveTo);
+
+            // Si el arreglo no tiene nodos, significa que no hay conexion entre los nodos
+            if(size == 0){
+                textMensaje.setText("No hay conexion entre los nodos");
+            }else{
+                for(int i = 1; i <= size; i++) {
+                    int coordX = pathNodos.get(size - i).getX() - 210; //offset de coordenadas
+                    int coordY = pathNodos.get(size - i).getY() - 550;
+                    LineTo newLine = new LineTo(coordX, coordY);
+                    path.getElements().add(newLine);
+                }
             }
+            // Camino en coordenadas en las que el nodo de la hormiga se mueve
+            PathTransition transition = new PathTransition();
+            transition.setNode(greenAnt);
+            transition.setDuration(Duration.seconds(6));
+            transition.setPath(path);
+            transition.setCycleCount(1);
+            transition.play();
+
+
+            end.setComida(false);
+            end = graphWeighted.nodoConComida();
+            gamePane.getChildren().remove(comida);
+
         }
 
-
-
-        //Creating the path transition
-        PathTransition transition = new PathTransition();
-        transition.setNode(greenAnt);
-        transition.setDuration(Duration.seconds(6));
-        transition.setPath(path);
-        transition.setCycleCount(1);
-        transition.play();
 
 
     }
